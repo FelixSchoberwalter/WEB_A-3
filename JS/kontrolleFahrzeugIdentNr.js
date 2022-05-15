6db321015463426a4d098c95797fde54a666305e
vcoelsch <2021488@stud.hs-mannheim.de>
2022-05-15 21:38:17 +0200

    Pruefung nun erledigt: Habe mir das Leben sehr kompliziert gemacht. Autsch

--- JS/kontrolleFahrzeugIdentNr.js
+++ JS/kontrolleFahrzeugIdentNr.js
@@ -2,7 +2,7 @@ const formHinzufuegen = document.getElementById("hinzufuegen");
 const fahrgestellnummer = document.getElementById("fahrgestellnummer");
 const bezeichnung = document.getElementById("bezeichnung")
 
-const regExFahrgestellnummer = /^[ABCDEFGHJKLMNPRSTUVWXYZ0-9]+$/;
+const regExFahrgestellnummer = /[QIO\s]+/;
 
 formHinzufuegen.addEventListener("submit", (e)=> {
 
@@ -15,10 +15,11 @@ formHinzufuegen.addEventListener("submit", (e)=> {
 function validiereFahrgestellnummer(){
     let fehlermeldung = [];
 
-    if(regExFahrgestellnummer.test(fahrgestellnummer.value) === false){
-        fehlermeldung.push("Ihre Fahrgestellnummer erfüllt die falschen Vorraussetzungen.");
-    }else if(fahrgestellnummer.value.length == 17){
-         fehlermeldung.push("Ihre Fahrgestellnummer erfüllt die falschen Vorraussetzungen.");
+    if(regExFahrgestellnummer.test(fahrgestellnummer.value) === true){
+        fehlermeldung.push("Ihre Fahrgestellnummer darf keine Sonderzeichen enthalten, keine Kleinbuchstaben und kein Q,I oder O.");
+    }
+    if(fahrgestellnummer.value.length !== 17){
+         fehlermeldung.push("Ihre Fahrgestellnummer hat nicht die richtige Länge. Diese musss 17 sein.");
     }
     if(bezeichnung.value.length < 1){
         fehlermeldung.push("Die Bezeichnung braucht einen Eintrag.");
